4e7d9c5bfc62ffb4f4d9e4fa2f2df2bca497780d
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-06-09 17:19:41 +0600

    rendered user image and name dynamically.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { DUMMY_fUSERS } from "../../dummy-users";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_fUSERS.length);
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+	selector: 'app-user',
+	standalone: true,
+	imports: [],
+	templateUrl: './user.component.html',
+	styleUrl: './user.component.css'
 })
 export class UserComponent {
+	selectedUser = DUMMY_fUSERS[randomIndex];
 
+	get imagePath () {
+		return 'assets/users/' + this.selectedUser.avatar;
+	}
 }
