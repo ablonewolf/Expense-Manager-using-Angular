7ac7af72e6b370ca65bea879bb8a34e755faa798
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-06-15 11:53:33 +0600

    outputted the task data in the task component and showed all the task data based on the user id.

---
+++ src/app/tasks/Tasks.ts
@@ -0,0 +1,30 @@
+export const Tasks = [
+	{
+		id: 't1',
+		userId: 'u1',
+		title: 'Master Java',
+		summary: 'Learn all the basic & advanced topics of Java & use it as a primary programming language.',
+		dueDate: '2025-12-31'
+	},
+	{
+		id: 't2',
+		userId: 'u2',
+		title: 'Learn Spring Boot',
+		summary: 'Understand the concepts of Spring Boot & Spring Framework in detail & prepare an MVC web application.',
+		dueDate: '2025-10-31'
+	},
+	{
+		id: 't3',
+		userId: 'u3',
+		title: 'Learn JavaScript',
+		summary: 'Master the core concepts of JavaScript & why it is essential for developing web applications',
+		dueDate: '2025-08-31'
+	},
+	{
+		id: 't4',
+		userId: 'u1',
+		title: 'Master Angular',
+		summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
+		dueDate: '2025-08-31'
+	}
+]
