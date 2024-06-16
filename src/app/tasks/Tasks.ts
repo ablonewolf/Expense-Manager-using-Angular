43ba4ae635262d24ff450d43cf7391f39d17c0d0
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-06-16 11:46:08 +0600

    changed the value of Id of all tasks.

--- src/app/tasks/Tasks.ts
+++ src/app/tasks/Tasks.ts
@@ -1,27 +1,27 @@
 export const Tasks = [
 	{
-		id: 't1',
+		id: '1',
 		userId: 'u1',
 		title: 'Master Java',
 		summary: 'Learn all the basic & advanced topics of Java & use it as a primary programming language.',
 		dueDate: '2025-12-31'
 	},
 	{
-		id: 't2',
+		id: '2',
 		userId: 'u2',
 		title: 'Learn Spring Boot',
 		summary: 'Understand the concepts of Spring Boot & Spring Framework in detail & prepare an MVC web application.',
 		dueDate: '2025-10-31'
 	},
 	{
-		id: 't3',
+		id: '3',
 		userId: 'u3',
 		title: 'Learn JavaScript',
 		summary: 'Master the core concepts of JavaScript & why it is essential for developing web applications',
 		dueDate: '2025-08-31'
 	},
 	{
-		id: 't4',
+		id: '4',
 		userId: 'u1',
 		title: 'Master Angular',
 		summary: 'Learn all the basic and advanced features of Angular & how to apply them.',
