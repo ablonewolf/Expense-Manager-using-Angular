62db77df017c10047cf2ef18937a2dbc257de9dc
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-06-16 21:48:01 +0600

    enabled content projection and created a wrapper card component to make the rounded border styles available globally.

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type User } from "./user.model";
+import { CardComponent } from "../utility-components/card/card.component";
 
 
 /*const getRandomIndex = () => {
@@ -9,7 +10,9 @@ import { type User } from "./user.model";
 @Component({
 	selector: 'app-user',
 	standalone: true,
-	imports: [],
+	imports: [
+		CardComponent
+	],
 	templateUrl: './user.component.html',
 	styleUrl: './user.component.css'
 })
