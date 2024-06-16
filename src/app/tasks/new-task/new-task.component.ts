6c0f86e0ea85f1366d32269c7f519be65a79a4a9
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-06-16 11:47:39 +0600

    added a submit handler using ngSubmit while submitting new task and emitted that data using an output attribute.

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,16 +1,28 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+import { newTask } from "./newtask.model";
 
 @Component({
 	selector: ' app-new-task',
 	standalone: true,
-	imports: [],
+	imports: [FormsModule],
 	templateUrl: './new-task.component.html',
 	styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
 	@Output() closeDialog = new EventEmitter<void>();
+	@Output() addNewTask = new EventEmitter<newTask>();
+	newTask: newTask = {
+		enteredTitle: '',
+		enteredSummary: '',
+		enteredDate: ''
+	};
 
 	onCloseDialog() {
 		this.closeDialog.emit();
+	}
+
+	onSubmit() {
+		this.addNewTask.emit(this.newTask);
 	}
 }
