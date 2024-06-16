ded91d7f44cc8b20df4e15ac6dbc9caffe950b7d
Arka Bhuiyan <arkabhuiyan66@gmail.com>
2024-06-16 22:39:38 +0600

    refactored the entire component codebase and moved all the components under the component directory and adjusted changes accordingly.

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../header/header.component";
 import { UserComponent } from "./user/user.component";
-import { DummyUsers } from "../dummy-users";
+import { DummyUsers } from "../../dummy-users";
 import { TasksComponent } from "./tasks/tasks.component";
+import { HeaderComponent } from "./header/header.component";
 
 @Component({
 	selector: 'app-root',
 	standalone: true,
-	imports: [HeaderComponent, UserComponent, TasksComponent],
+	imports: [HeaderComponent, UserComponent, TasksComponent, HeaderComponent],
 	templateUrl: './app.component.html',
 	styleUrl: './app.component.css',
 })
